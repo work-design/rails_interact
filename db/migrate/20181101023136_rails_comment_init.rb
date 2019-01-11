38b1769f075e08715d64a3c680da3d76258ed8aa
qinmingyuan <mingyuan0715@foxmail.com>
2019-01-11 18:03:24 +0800

    rails comment

---
+++ db/migrate/20181101023136_rails_comment_init.rb
@@ -0,0 +1,31 @@
+class RailsCommentInit < ActiveRecord::Migration[5.2]
+  def change
+
+    create_table :comments do |t|
+      t.references :commentable, polymorphic: true
+      t.references :commenter
+      t.references :parent
+      t.string :title
+      t.text :content
+      t.string :state
+      t.float :score
+      t.integer :liked_count
+      t.integer :star_count
+      t.timestamps
+    end
+
+    create_table :attitudes do |t|
+      t.references :attitudinal, polymorphic: true
+      t.references :user
+      t.string :opinion
+      t.timestamps
+    end
+
+    create_table :stars do |t|
+      t.references :starred, polymorphic: true
+      t.references :user
+      t.timestamps
+    end
+
+  end
+end
